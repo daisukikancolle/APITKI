5a9be5ddadbc1a37d0556b8e5b3b58775745cbe0
Billy Anderson Matjik <daisukikancolle@gmail.com>
2020-06-19 22:01:15 +0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -7,19 +7,48 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('a/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
+    // $app->get('a/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+    //     // Sample log message
+    //     $container->get('logger')->info("Slim-Skeleton '/' route");
 
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
+    //     // Render index view
+    //     return $container->get('renderer')->render($response, 'index.phtml', $args);
+    // });
 
-    $app->get('/', function ($request, $response, $args) {
-        $sth = $this->db->prepare("SELECT * FROM tabel_pegawai");
+    $app->post('/loginuser', function ($request, $response, $args) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+
+        $sth = $this->db->prepare("SELECT a.id_user as iduser,a.username as username FROM tbl_user AS a WHERE a.username = :username AND a.password = :password");
+        $sth ->bindParam(':username',$username);
+        $sth ->bindParam(':password',$password);
         $sth->execute();
         $datas = $sth->fetchAll();
-        return $this->response->withJson($datas);
+        return $this->response->withJson($datas[0]);
+    });
+
+    $app->post('/loginpegawai', function ($request, $response, $args) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+
+        $sth = $this->db->prepare("SELECT a.id_pegawai as iduser,a.username as username FROM tabel_pegawai AS a WHERE a.username = :username AND a.password = :password");
+        $sth ->bindParam(':username',$username);
+        $sth ->bindParam(':password',$password);
+        $sth->execute();
+        $datas = $sth->fetchAll();
+        return $this->response->withJson($datas[0]);
+    });
+
+    $app->post('/loginpelatih', function ($request, $response, $args) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+
+        $sth = $this->db->prepare("SELECT a.id_pelatih as iduser,a.username as username FROM tabel_pelatih AS a WHERE a.username = :username AND a.password = :password");
+        $sth ->bindParam(':username',$username);
+        $sth ->bindParam(':password',$password);
+        $sth->execute();
+        $datas = $sth->fetchAll();
+        return $this->response->withJson($datas[0]);
     });
     
 };
